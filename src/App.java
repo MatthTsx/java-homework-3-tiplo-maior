3853f1adb6ae21b950e7cbf79a900af00adc1996
RobertinCarlos <shakii876954@gmail.com>
2023-04-25 08:22:20 -0300

    java homework Triplo maior

---
+++ src/App.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        boolean process = true;
+
+        while(process){
+            String input = scan.nextLine();
+            String[] numbers_str = input.split("\\s+");
+            int[] numbers = {};
+
+            for (String number_str : numbers_str) {
+                int[] new_arry = Arrays.copyOf(numbers, numbers.length + 1);
+                new_arry[new_arry.length - 1] = Integer.parseInt(number_str);
+                numbers = new_arry;
+            }
+
+            int maior = numbers[0];
+            for (int number : numbers) {
+                if(number > maior){
+                    maior = number;
+                }
+            }
+            System.out.println(maior);
+            System.out.println("continuar? n/s");
+            if(scan.nextLine().toLowerCase().equals("n")){
+                process = false;
+                break;
+            }
+
+        }
+        scan.close();
+    }
+}
